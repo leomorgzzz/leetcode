bb567037e4aeaa58c1f0824edd1b5eda5253fbb5
leo's room <leomoralesrgz@gmail.com>
2025-01-15 03:42:17 +0000

    update 1

---
+++ twosum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (seen.find(complement) != seen.end()) {
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {}; 
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices encontrados: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No se encontraron dos números que sumen el objetivo." << endl;
+    }
+
+    return 0;
+}
